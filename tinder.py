1f3e5478e935774494710fccaa7e1fc542394ace
Cyrus Sethna <csethna@gmail.com>
2018-03-09 19:50:31 -0500

    initial commit

---
+++ tinder.py
@@ -0,0 +1,10 @@
+from credentials import *
+import pynder
+
+session = pynder.Session(facebook_id=FACEBOOK_ID, facebook_token=FACEBOOK_AUTH_TOKEN) #kwargs
+session.matches() # get users you have already been matched with
+#session.update_location(LAT="39.011357", LON="-77.056249") # updates latitude and longitude for your profile
+session.profile  # your profile. If you update its attributes they will be updated on Tinder.
+users = session.nearby_users() # returns a iterable of users nearby
+
+print(users)
